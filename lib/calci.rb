e6aedc508d5e055fb88dd65c49ec842c45a5166f
sapna parmar <sapna.parmar05@gmail.com>
2024-03-01 00:40:33 +0530

    added specs for adding numbers in string with cutom delimeter

--- lib/calci.rb
+++ lib/calci.rb
@@ -1,7 +1,18 @@
 class Calci
 
   def add(str)
-    numbers = str.split(',')
+    return 0 if str.empty?
+    array = str.split(/\r?\n/)
+    puts array
+    del = if array.first.include?('//')
+      array.first[-1,1]
+    else
+      ','
+    end
+    array.shift
+    # array = str.split(',').map{|e| e.split(/\r?\n/)}.flatten
+    numbers = array.map{ |e| e.split(del) }.flatten
+    puts numbers
     return numbers.inject(0){ |sum, e| sum + e.to_i}
   end
 end
--- spec/calci_spec.rb
+++ spec/calci_spec.rb
@@ -2,7 +2,7 @@ require './lib/calci.rb'
 
 describe Calci do
 
-  before(:each) do
+  before do
     @calc = Calci.new
   end
 
@@ -15,6 +15,16 @@ describe Calci do
 
     it "should return sum of numbers" do
       sum = @calc.add("1,2")
+      expect(sum).to eq(3)
+    end
+
+    it "should return sum of numbers if new line is given in string" do
+      sum = @calc.add("1\n2")
+      expect(sum).to eq(3)
+    end
+
+    it "should return sum of numbers if custom delimeter is given" do
+      sum = @calc.add("//;\n1\n2")
       expect(sum).to eq(3)
     end
   end
